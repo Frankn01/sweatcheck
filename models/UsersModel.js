89762274254f67cfbcec42b6232666206002cf96
Noah Frank <frankn@knights.ucf.edu>
2022-07-07 01:22:30 -0400

    Fix: bcrypt added

---
+++ models/UsersModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+//note first and last are not required to login
+const usersSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    login: String,
+    password: String
+}, { versionKey: false })   //versionKey: false -> gets rid of _v when adding to a collection
+
+// the name you send below is the name that the collection will be called
+var collectionName = 'Users'
+module.exports = mongoose.model('User',usersSchema,collectionName)
+
---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+const loginRoutes = require('./routes/login')
+const registerRoutes = require('./routes/register')
+const userRoutes = require('./routes/User')
+
+const path = require('path')
+const mongoose  = require('mongoose')
+const app = express() //express app
+
+//middleware
+app.use(express.json())
+app.use((req,res,next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.use('/api/workouts',workoutRoutes)
+app.use('/api/login',loginRoutes)
+app.use('/api/register',registerRoutes)
+app.use('/api/user',userRoutes)
+
+
+
+//connect using mongoose
+const conn = mongoose.connect(process.env.MONGO_URI)
+.then(()=>{ 
+    app.listen(process.env.PORT, ()=>{
+        console.log('connected to db & listening on port',process.env.PORT)
+    })
+})
+.catch((error)=>{
+    console.log(error)
+})
