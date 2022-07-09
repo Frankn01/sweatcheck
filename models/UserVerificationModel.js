62bf1105ef50e833c1f845870467eb9d3b5dc2c7
Noah Frank <frankn@knights.ucf.edu>
2022-07-08 21:42:04 -0400

    Added wrong file opps

---
+++ models/UserVerificationModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+//note first and last are not required to login
+const usersVerificationSchema = new Schema({
+    userId: {
+        type: String 
+    },
+    uniqueString: {
+        type: String
+    },
+    createdAt: {
+        type: Date
+    },
+    expiresAt: {
+        type: Date
+    }
+    
+}, { versionKey: false })   //versionKey: false -> gets rid of _v when adding to a collection
+
+// the name you send below is the name that the collection will be called
+var collectionName = 'Users'
+module.exports = mongoose.model('User',usersSchema,collectionName)
+
