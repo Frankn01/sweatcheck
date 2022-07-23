0963f0d326f6625848f60f66ae0d89f00b425601
aclargo <55463638+aclargo@users.noreply.github.com>
2022-07-23 17:26:10 -0400

    Delete shared.js

--- shared.js
+++
@@ -1,3 +0,0 @@
-export const baseAPIUrl = 'https://sweatcheck.herokuapp.com';
-export const allWorkoutAPI = 'https://sweatcheck.herokuapp.com/api/workouts/all/'
-//export const baseAPIUrl = 'https://evening-peak-89288.herokuapp.com';
